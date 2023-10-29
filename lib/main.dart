6a4203dfd7880a7912fab1e03ac6b02260c2554a
mawalou14 <awaloum213@gmail.com>
2023-10-29 01:50:19 +0100

    created and navigating between screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/screens/create_room_screen.dart';
+import 'package:tictactoe/screens/join_room_screen.dart';
 import 'package:tictactoe/screens/main_menu_screen.dart';
 import 'package:tictactoe/utils/colors.dart';
 
@@ -17,6 +19,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: bgColor,
       ),
+      routes: {
+        MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+        JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+        CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+      },
       home: const MainMenuScreen(),
     );
   }
--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/responsive/responsive.dart';
+import 'package:tictactoe/screens/create_room_screen.dart';
+import 'package:tictactoe/screens/join_room_screen.dart';
 import 'package:tictactoe/widgets/custom_button.dart';
 
 class MainMenuScreen extends StatelessWidget {
+  static String routeName = '/main-menu';
   const MainMenuScreen({super.key});
+
+  void createRoom(BuildContext context) {
+    Navigator.pushNamed(context, CreateRoomScreen.routeName);
+  }
+
+  void joinRoom(BuildContext context) {
+    Navigator.pushNamed(context, JoinRoomScreen.routeName);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +24,14 @@ class MainMenuScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CustomButton(
-              onTap: () {},
+              onTap: () => createRoom(context),
               text: 'Create Room',
             ),
             const SizedBox(
               height: 20,
             ),
             CustomButton(
-              onTap: () {},
+              onTap: () => joinRoom(context),
               text: 'Join Room',
             ),
           ],
