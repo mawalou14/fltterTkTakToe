14b7300faf792ceb04eef6f5a22c5e9424615cb0
mawalou14 <awaloum213@gmail.com>
2023-11-04 00:28:26 +0100

    successfully connected to mongo Db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT|| 3000;
@@ -12,6 +13,16 @@ var io = require("socket.io")(server);
 
 // Middleware
 app.use(express.json());
+
+const DB = "mongodb+srv://mawalou14:6292568Lufab@cluster0.eyhqffa.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection Successfull");
+})
+.catch((e) => {
+    console.log(e);
+});
+
 
 server.listen(port, '0.0.0.0', () => {
     console.log('Server started and running on port ' + port);
