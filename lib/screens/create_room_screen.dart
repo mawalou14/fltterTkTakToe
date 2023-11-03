48e705f0f7ec17ac9249246c9899ae0498d2ab26
mawalou14 <awaloum213@gmail.com>
2023-11-03 05:49:42 +0100

    Creat room screem okay

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/responsive/responsive.dart';
 import 'package:tictactoe/widgets/custom_button.dart';
 import 'package:tictactoe/widgets/custom_text.dart';
 import 'package:tictactoe/widgets/custom_textfield.dart';
@@ -23,29 +24,31 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CustomText(
-              shadows: [Shadow(blurRadius: 40, color: Colors.blue)],
-              text: 'Create Room',
-              fontSize: 70,
-            ),
-            SizedBox(
-              height: size.height * 0.08,
-            ),
-            CustomTextField(
-                controller: _nameController, hintText: 'Enter your nickname'),
-            SizedBox(
-              height: size.height * 0.08,
-            ),
-            CustomButton(onTap: () {}, text: 'Create')
-          ],
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [Shadow(blurRadius: 40, color: Colors.blue)],
+                text: 'Create Room',
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomTextField(
+                  controller: _nameController, hintText: 'Enter your nickname'),
+              SizedBox(
+                height: size.height * 0.045,
+              ),
+              CustomButton(onTap: () {}, text: 'Create')
+            ],
+          ),
         ),
       ),
     );
