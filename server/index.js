5ceb34c69a1b3966f2baf9414a61aeb44ade3116
mawalou14 <awaloum213@gmail.com>
2023-11-04 00:13:31 +0100

    initialize and started express app on port 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('Hello it is Awalou');
+
+
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const port = process.env.PORT|| 3000;
+
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+// Middleware
+app.use(express.json());
+
+server.listen(port, '0.0.0.0', () => {
+    console.log('Server started and running on port ' + port);
+})
