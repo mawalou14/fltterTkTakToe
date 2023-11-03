80d9d8b568211413296fd4b1289419755e44479c
mawalou14 <awaloum213@gmail.com>
2023-11-03 06:00:33 +0100

    Join room screen okay

--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../responsive/responsive.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_textfield.dart';
+
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = '/join-room';
   const JoinRoomScreen({super.key});
@@ -9,8 +14,51 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _gameIdController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _gameIdController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [Shadow(blurRadius: 40, color: Colors.blue)],
+                text: 'Join Room',
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomTextField(
+                  controller: _nameController, hintText: 'Enter your nickname'),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              CustomTextField(
+                  controller: _gameIdController, hintText: 'Enter Game ID'),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(onTap: () {}, text: 'Join')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
