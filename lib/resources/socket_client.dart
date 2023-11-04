e1637e63b9dc917d2086a7001ebdc650f3315b96
mawalou14 <awaloum213@gmail.com>
2023-11-04 02:57:27 +0100

    connected to socket

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -1,10 +1,19 @@
-// import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-// class SocketClient {
-//   IO.Socket? socket;
-//   static SocketClient _instance;
+class SocketClient {
+  IO.Socket? socket;
+  static SocketClient? _instance;
 
-//   SocketClient._instance() {
-//     socket = IO.io('');
-//   }
-// }
+  SocketClient._internal() {
+    socket = IO.io('http://192.168.57.90:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket!.connect();
+  }
+
+  static SocketClient get instance {
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
+}
