a4783e753d7e2a28e9ef6f1daf26937f5775eb9a
mawalou14 <awaloum213@gmail.com>
2023-11-05 23:56:51 +0100

    create room screen connected to socket

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/resources/socket_methods.dart';
 import 'package:tictactoe/responsive/responsive.dart';
 import 'package:tictactoe/widgets/custom_button.dart';
 import 'package:tictactoe/widgets/custom_text.dart';
@@ -14,6 +15,9 @@ class CreateRoomScreen extends StatefulWidget {
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
   final TextEditingController _nameController = TextEditingController();
+final SocketMethods _socketMethods = SocketMethods();
+
+
   @override
   void dispose() {
     super.dispose();
@@ -46,7 +50,7 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
               SizedBox(
                 height: size.height * 0.045,
               ),
-              CustomButton(onTap: () {}, text: 'Create')
+              CustomButton(onTap: () => _socketMethods.createRoom(_nameController.text), text: 'Create')
             ],
           ),
         ),
