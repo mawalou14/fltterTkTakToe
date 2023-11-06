41a9f8f27aa3869883e383b5abf8a0ea7b0cfb9f
mawalou14 <awaloum213@gmail.com>
2023-11-06 23:43:07 +0100

    created player schema

---
+++ server/models/player.js
@@ -0,0 +1,20 @@
+
+const mongodb = require('mongodb');
+
+const playerSchema = new mongoose.Schema({
+    nickname: {
+        type: String,
+        trim: true,
+    },
+    socketID: {
+        type: String,
+    },
+    points: {
+        type: Number,
+        default: 0,
+    },
+    playerType: {
+        required: true,
+        type: String,
+    }
+});
