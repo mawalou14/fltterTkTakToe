f3066c733881471702bc3825fe36cd443e59e4a3
mawalou14 <awaloum213@gmail.com>
2023-11-08 23:50:14 +0100

    create room successfull and printing socket info

--- server/index.js
+++ server/index.js
@@ -23,7 +23,6 @@ io.on("connection", (socket) => {
     socket.on("createRoom", async ({ nickname }) => {
      console.log(nickname);
      try {
-
     //  Room is created
     let room = new Room();
     let player = {
